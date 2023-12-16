ed723b140bfefceb8c6f9f385e452a3c22468eb9
Jacky Song <almey0721@gmail.com>
2023-12-15 23:48:02 -0500

    Added graphviz export for debugging

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,16 +1,28 @@
-use elaraui::{UI, Label};
+use elaraui::{UI, Label, Layout};
 
 fn main() {
     let mut ui = UI::new(1600, 1200);
     // as ratio, that is a 3-5-3 ratio of widths
-    ui.set_layout_row([3, 5, 3]);
+    // ui.set_layout_row([3, 5, 3]);
     // as percent, so 10 = 10%
-    ui.set_padding([10, 10, 10, 10]);
+    // ui.set_padding([10]);
     let label_left = Label::new("Left sidebar");
+    ui.add_element(label_left);
+    
     let label_center = Label::new("Center area");
-    let label_right = Label::new("Right sidebar");
-    ui.add(label_left);
-    ui.add(label_center);
-    ui.add(label_right);
-    println!("{:#?}", ui);
+    ui.add_element(label_center);
+    
+    // let label_right = Label::new("Right sidebar");
+    let mut right_label_container = Layout::new_rows([2, 2]);
+    
+    let right_label_top = Label::new("Right top");
+    right_label_container.add_element(right_label_top);
+    
+    let right_label_bottom = Label::new("Right bottom");
+    right_label_container.add_element(right_label_bottom);
+    
+    ui.add_layout(right_label_container);
+    // println!("{:#?}", ui);
+    let s = ui.to_graphviz();
+    println!("{}", s);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
+// any future modules
+// in lib go here
+
 use std::fmt;
 use std::sync::atomic::AtomicUsize;
+static VAL_CNT: AtomicUsize = AtomicUsize::new(0);
 
 pub trait NodeType {
 	fn node_type(&self) -> &str;
+}
+
+pub trait HasID {
+	fn id(&self) -> usize;
 }
 
 #[derive(Debug)]
 pub enum Element {
 	ChildLayout(Layout),
 	ChildComponent(Box<dyn Component>)
+}
+
+impl NodeType for Element {
+	fn node_type(&self) -> &str {
+		match self {
+			Self::ChildLayout(layout) => layout.node_type(),
+			Self::ChildComponent(boxed_component) => &boxed_component.node_type()
+		}
+	}
+}
+
+impl HasID for Element {
+	fn id(&self) -> usize {
+		match self {
+			Self::ChildLayout(layout) => layout.id(),
+			Self::ChildComponent(boxed_component) => boxed_component.id()
+		}
+	}
 }
 
 #[derive(Debug)]
@@ -20,6 +46,7 @@ pub struct Layout
     layout_type: LayoutType,
     proportions: Vec<u32>,
     padding: Vec<u32>,
+    id: usize
 }
 
 impl NodeType for Layout {
@@ -34,36 +61,37 @@ pub enum LayoutType {
     Row
 }
 
-// Work on to_graphviz()
-
 impl Layout {
     pub fn empty() -> Layout {
-    	// let id = VAL_CNT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+    	let id = VAL_CNT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         Layout {
             elements: vec![],
             layout_type: LayoutType::Column,
             proportions: vec![1],
             padding: vec![0],
+            id
         }
     }
 
     pub fn new_rows<const N: usize>(proportions: [u32; N]) -> Layout {
-    	// let id = VAL_CNT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+    	let id = VAL_CNT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         Layout {
             elements: vec![],
             layout_type: LayoutType::Row,
             proportions: proportions.to_vec(),
             padding: vec![0],
+            id
         }
     }
     
     pub fn new_columns<const N: usize>(proportions: [u32; N]) -> Layout {
-    	// let id = VAL_CNT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+    	let id = VAL_CNT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         Layout {
             elements: vec![],
             layout_type: LayoutType::Column,
             proportions: proportions.to_vec(),
             padding: vec![0],
+            id
         }
     }
     
@@ -87,18 +115,9 @@ impl Layout {
     pub fn set_layout_type(&mut self, layout_type: LayoutType) {
         self.layout_type = layout_type;
     }
-    
-    // Calculate the (x, y) locations
-    // of each component based on the
-    // layout tree and propagate the
-    // location to each component
-    fn calculate_locations(&mut self) {
 
-    }
-    
-    pub fn render(&mut self) {
-        // TODO
-        self.calculate_locations();
+    pub fn id(&self) -> usize {
+    	self.id
     }
 }
 
@@ -107,13 +126,7 @@ pub struct UI
 {
     width: u32,
     height: u32,
-    elements: Layout
-}
-
-impl NodeType for UI {
-	fn node_type(&self) -> &str {
-		"UI"
-	}
+    container: Element
 }
 
 impl UI
@@ -122,56 +135,85 @@ impl UI
         UI {
             width,
             height,
-            elements: Layout::empty()
+            container: Element::ChildLayout(Layout::empty())
         }
     }
     
     pub fn set_layout_column<const N: usize>(&mut self, proportions: [u32; N]) {
-        self.elements.set_layout(proportions);
-        self.elements.set_layout_type(LayoutType::Column);
-    }
-    
-    pub fn set_layout_row<const N: usize>(&mut self, proportions: [u32; N]) {
-        self.elements.set_layout(proportions);
-        self.elements.set_layout_type(LayoutType::Row);
-    }
-    
-    pub fn add<C: Component + 'static>(&mut self, element: C) {
-        self.elements.add_element(element)
-    }
-    
-    pub fn add_layout_container(&mut self, layout: Layout) {
-        self.elements.add_layout(layout);
-    }
-    
-    pub fn set_padding(&mut self, padding: [u32; 4]) {
-        self.elements.set_padding(padding);
+    	if let Element::ChildLayout(ref mut container) = &mut self.container {
+    	    container.set_layout(proportions);
+    	    container.set_layout_type(LayoutType::Column);
+    	}
     }
 
-    pub fn to_graphviz(&self) {
-    	// self.elements.
+     pub fn set_layout_row<const N: usize>(&mut self, proportions: [u32; N]) {
+    	if let Element::ChildLayout(ref mut container) = &mut self.container {
+    	    container.set_layout(proportions);
+    	    container.set_layout_type(LayoutType::Row);
+    	}
+    }
+
+    pub fn set_padding(&mut self, padding: [u32; 4]) {
+    	if let Element::ChildLayout(ref mut container) = &mut self.container {
+    	    container.set_padding(padding);
+    	}
+    }
+ 
+    pub fn add_element<C: Component + 'static>(&mut self, element: C) {
+    	if let Element::ChildLayout(ref mut container) = &mut self.container {
+    		container.add_element(element);
+    	}
+    }
+
+    pub fn add_layout(&mut self, layout: Layout) {
+    	if let Element::ChildLayout(ref mut container) = &mut self.container {
+    		container.add_layout(layout);
+    	}
+    }
+
+    pub fn to_graphviz(&self) -> String {
+    	fn inner(element: &Element) -> String {
+    		let node_type = if element.id() == 0 { "UI" } else { element.node_type() };
+    		let mut s = format!("q{} [label=\"{}\"];\n", element.id(), node_type);
+    		if let Element::ChildLayout(layout) = element {
+	    		for el in layout.elements.iter() {
+		    			s.push_str(&inner(el));
+		    			s.push_str(&format!("q{} -- q{};\n", element.id(), el.id()));
+		    		}	
+	    		} 		
+    		s
+    	}
+    	let mut s_graph = String::from("graph {\n");
+    	s_graph.push_str(&inner(&self.container));
+    	s_graph.push_str("}\n");
+    	s_graph
+    }
+
+    // Calculate the (x, y) locations and widths
+    // of each component based on the
+    // layout tree and propagate the
+    // location to each component
+    fn calculate_sizes_positions(&mut self) {
+    	// something like self.iter().calculate_frame() for each element
     }
 }
 
 
 pub struct Button {
     label: String,
-    location: Option<(f32, f32)>,
+    location: Option<(f32, f32)>, // anchor point
     dimensions: Option<(f32, f32)>, // (width, height)
-}
-
-impl NodeType for Button {
-	fn node_type(&self) -> &str {
-		"Button"
-	}
+    id: usize
 }
 
 impl Button {
     pub fn new(label: &str) -> Button {
+    	let id = VAL_CNT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         Button {
             label: label.to_string(),
             location: None,
             dimensions: None,
+            id
         }
     }
 }
@@ -188,28 +230,33 @@ impl Component for Button {
     fn get_computed_location(&self) -> Option<&(f32, f32)> {
         self.location.as_ref()
     }
+
+    fn node_type(&self) -> &str {
+    	"Button"
+    }
+
+    fn id(&self) -> usize {
+       	self.id
+    }
 }
 
 pub struct Label {
     name: String,
     location: Option<(f32, f32)>,
     dimensions: Option<(f32, f32)>,
+    id: usize
 }
 
 impl Label {
     pub fn new(name: &str) -> Label {
+    	let id = VAL_CNT.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         Label {
             name: name.to_string(),
             location: None,
             dimensions: None,
+            id
         }
     }
-}
-
-impl NodeType for Label {
-	fn node_type(&self) -> &str {
-		"Label"
-	}
 }
 
 impl Component for Label {
@@ -224,12 +271,22 @@ impl Component for Label {
     fn get_computed_location(&self) -> Option<&(f32, f32)> {
         self.location.as_ref()
     }
+
+    fn node_type(&self) -> &str {
+    	"Label"
+    }
+
+    fn id(&self) -> usize {
+    	self.id
+    }
 }
 
 pub trait Component {
     fn name(&self) -> &'static str;
     fn set_computed_location(&mut self, location: (f32, f32));
     fn get_computed_location(&self) -> Option<&(f32, f32)>;
+    fn node_type(&self) -> &str;
+    fn id(&self) -> usize;
     // will be implemented with elara-gfx drawing backend
     // fn draw(&self, tr: TextRenderer, lr: LineRenderer, rr: RectRenderer); 
 }
